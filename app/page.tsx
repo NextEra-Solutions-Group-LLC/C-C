d62bb2b2515d03f10cf5090bf2ce2db69b774de0
Imran Al Farabi <imranalfarabidevworks@gmail.com>
2026-09-19 00:14:06 +0600

    added soem

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import FloatingActions from "./components/Floating";
 
 export default function Home() {
   return (
     <div>
 
       <Hero />
-      <FloatingActions />
+
     </div>
   );
 }
